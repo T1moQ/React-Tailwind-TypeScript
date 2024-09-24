8c2cb3236311051f748486653feec81c352497f0
Timofey <pttima@yandex.ru>
2024-09-24 22:50:53 +0300

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		screens: {
+			xl: '1440px',
+			md: '375px',
+		},
+		colors: {
+			'almost-white': 'hsl(0, 0%, 98%)',
+			'medium-grey': 'hsl(0, 0%, 41%)',
+			'almost-black': 'hsl(0, 0%, 8%)',
+		},
 		extend: {},
 	},
 	plugins: [],
